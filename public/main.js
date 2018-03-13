44910ddee6cad3c3c94a5e6b2e77d6aaffb111e3
Mack <amack@proforma.com>
2018-03-13 14:13:45 -0400

    added if statements to computer

--- public/main.js
+++ public/main.js
@@ -1,5 +1,21 @@
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  document.querySelector('h1').textContent += ''
+
+  const user = prompt("Rock, Paper, or Scissors?");
+  const computer = Math.random();
+
+  if (computer <= 0.33) {
+    computer = "rock";
+  } else if (computer <= 0.66) {
+    computer = "paper";
+  } else {
+    computer = "scissors";
+  }
+  const compare = function(user, computer) {
+    if (user === computer) {
+      alert("It is a tie!");
+    }
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main)
