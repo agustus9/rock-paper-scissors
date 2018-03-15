63c5e3074df36e83f0be72595d7926a603ef61cf
Mack <amack@proforma.com>
2018-03-15 14:11:22 -0400

    refractered players to use objects

--- public/main.js
+++ public/main.js
@@ -2,17 +2,17 @@ const rock = 'Rock';
 const paper = 'Paper';
 const scissor = 'Scissor';
 
-let playerOne = '';
-let playerTwo = '';
+let playerOne = { choice: '' };
+let playerTwo = { choice: '' };
 
 const playerOneChoice = (choice) => {
   console.log(`Player 1 choose: ${choice}`);
-  playerOne = choice;
+  playerOne.choice = choice;
 }
 
 const playerTwoChoice = (choice) => {
   console.log(`Player 2 choose: ${choice}`);
-  playerTwo = choice;
+  playerTwo.choice = choice;
 }
 
 const main = () => {
